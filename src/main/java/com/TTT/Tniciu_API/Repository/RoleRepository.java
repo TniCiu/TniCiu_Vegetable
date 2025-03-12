a065d5975b0becefe1d8081c86884836da74dbcd
Huynh Trong Tin <135334306+TniCiu@users.noreply.github.com>
2025-03-12 10:54:52 +0700

    Update RoleRepository.java

--- src/main/java/com/TTT/Tniciu_API/Repository/RoleRepository.java
+++ src/main/java/com/TTT/Tniciu_API/Repository/RoleRepository.java
@@ -2,9 +2,9 @@ package com.TTT.Tniciu_API.Repository;
 
 import com.TTT.Tniciu_API.Model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
-
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
