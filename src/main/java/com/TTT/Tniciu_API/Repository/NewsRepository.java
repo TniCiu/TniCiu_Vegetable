a129aab452649dfb08fd5a9a4c938d9cbc36c0a8
Huynh Trong Tin <135334306+TniCiu@users.noreply.github.com>
2025-03-12 10:55:08 +0700

    Update NewsRepository.java

--- src/main/java/com/TTT/Tniciu_API/Repository/NewsRepository.java
+++ src/main/java/com/TTT/Tniciu_API/Repository/NewsRepository.java
@@ -1,7 +1,7 @@
 package com.TTT.Tniciu_API.Repository;
-
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.TTT.Tniciu_API.Model.New;
-
+@Repository
 public interface NewsRepository extends JpaRepository<New, Long> {
 }
